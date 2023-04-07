929696678f2dfe8556ee514c6deaa7ff0e03c9b7
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-04-08 00:35:07 +0300

    Reformat .py;

--- blog/migrations/0010_auto_20190913_2109.py
+++ blog/migrations/0010_auto_20190913_2109.py
@@ -4,17 +4,20 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('blog', '0009_auto_20190913_1713'),
+        ("blog", "0009_auto_20190913_1713"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='comment',
-            options={'ordering': ['published_at'], 'verbose_name': 'комментарий', 'verbose_name_plural': 'комментарии'},
+            name="comment",
+            options={
+                "ordering": ["published_at"],
+                "verbose_name": "комментарий",
+                "verbose_name_plural": "комментарии",
+            },
         ),
         migrations.DeleteModel(
-            name='Avatar',
+            name="Avatar",
         ),
     ]
